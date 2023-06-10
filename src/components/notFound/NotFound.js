08677173f3137963ab017cc47d8ff9fca0db28d4
Fezzopro <fezzopro@gmail.com>
2023-06-10 17:13:15 +0200

    Add 404 error component to the bookstore application

---
+++ src/components/notFound/NotFound.js
@@ -0,0 +1,14 @@
+import './NotFound.css';
+
+const NotFound = () => (
+  <div className="category-secion">
+    <h2>
+      <span className="not-found">404 </span>
+      <br />
+      OOPS... Page Not Found
+    </h2>
+    <span className="engineer-emoji"> 🕵️‍♂️ </span>
+  </div>
+);
+
+export default NotFound;
