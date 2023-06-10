4fdd4278e88260544cacfbd8d19f14c67a1ed307
Fezzopro <fezzopro@gmail.com>
2023-06-10 17:13:46 +0200

    Add category component to the bookstore application

---
+++ src/components/Categories/Category.js
@@ -0,0 +1,12 @@
+import './Category.css';
+
+const Category = () => (
+  <>
+    <div className="category-secion">
+      <h2>Category page Under Construction</h2>
+      <span className="engineer-emoji"> 👷 </span>
+    </div>
+  </>
+);
+
+export default Category;
