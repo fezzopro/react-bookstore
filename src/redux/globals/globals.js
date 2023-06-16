ae19be9038512b0362e1fd3d67769825b35e70db
fezzopro <fezzopro@gmail.com>
2023-06-16 22:13:42 +0200

    Add global variables like APP_URL and APP_TOKEN

---
+++ src/redux/globals/globals.js
@@ -0,0 +1,7 @@
+const API_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/';
+
+const APP_TOKEN = 'RKKRzIA40cTUd7jHebct';
+
+export { API_URL, APP_TOKEN };
+
+export default API_URL;
